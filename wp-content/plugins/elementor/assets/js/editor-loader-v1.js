abebc535ac0b042952e354957d18e6742e389fed
Ben Rich <hello@benrich.uk>
2025-05-21 13:34:18 +0100

    Updated/Added simply-static-1-1747830628

--- wp-content/plugins/elementor/assets/js/editor-loader-v1.js
+++ wp-content/plugins/elementor/assets/js/editor-loader-v1.js
@@ -1,12 +1,6 @@
 /*! elementor - v3.28.0 - 22-04-2025 */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+(()=>{"use strict";
 /*!*******************************************************!*\
   !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
   \*******************************************************/
-
-
-window.elementor.start();
-/******/ })()
-;
-//# sourceMappingURL=editor-loader-v1.js.map
+window.elementor.start();})();
